c8d56700861808b92fbea3b9984c4f121340297f
PUSANISA CHANNUAN <153055201+6504062636268@users.noreply.github.com>
2025-12-23 18:28:51 +0700

    Add files via upload

---
+++ ws1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d;
+	int max,second;
+	scanf("%d %d %d %d",&a,&b,&c,&d);
+	
+	max=(a>b? a:b);
+	max=(max>c? max:c);
+	max=(max>d? max:d);
+	
+	second= (a!=max?a:-2147483658);
+	second= ((b!=max&&b>second)?b:second);
+	second= ((c!=max&&c>second)?c:second);
+	second= ((d!=max&&d>second)?d:second);
+
+	printf("%d",second);
+	
+}
