bab5dec1fc378d5dd5a4ea84dabf535fc0aae65e
PUSANISA CHANNUAN <153055201+6504062636268@users.noreply.github.com>
2025-12-23 19:34:06 +0700

    Add files via upload

---
+++ ws2expr.c
@@ -0,0 +1,59 @@
+#include <math.h>
+int main() {
+	float  a,b,c,d;
+	float ans;
+	
+	a=1.0,b=1.0,c=5.0,d=1.0;
+	printf("a=1 b=1 c=5 d=1\n");
+	ans = ((3.0*(a/5.0))+(1.0/b));
+	printf("%.3f\n",ans);
+	
+	ans = ((3.0*a)+(5.0*b))/(2.0+c);
+	printf("%.3f\n",ans);
+	
+	ans = (2.0/7.0)*(pow(4.0,3.0+c)-5.0*d);
+	printf("%.3f\n",ans);
+	
+	ans = sqrt((2.0 + 8.0*b) / a);
+	printf("%.3f\n",ans);
+	
+	ans = cbrt(b*b-4*d);
+	printf("%.3f\n\n\n",ans);
+
+
+	a=9.0, b=2.0, c=0, d=1.0;
+	printf("a=9, b=2, c=0, d=1\n");
+	ans = ((3.0*(a/5.0))+(1.0/b));
+	printf("%.3f\n",ans);
+	
+	ans = ((3.0*a)+(5.0*b))/(2.0+c);
+	printf("%.3f\n",ans);
+	
+	ans = (2.0/7.0)*(pow(4.0,3.0+c)-5.0*d);
+	printf("%.3f\n",ans);
+	
+	ans = sqrt((2.0 + 8.0*b) / a);
+	printf("%.3f\n",ans);
+	
+	ans = cbrt(b*b-4*d);
+	printf("%.3f\n\n\n",ans);
+	
+	
+	a=0, b=3.0, c=3.0, d=3.0;
+	printf("a=0, b=3, c=3, d=3\n");
+	ans = ((3.0*(a/5.0))+(1.0/b));
+	printf("%.3f\n",ans);
+	
+	ans = ((3.0*a)+(5.0*b))/(2.0+c);
+	printf("%.3f\n",ans);
+	
+	ans = (2.0/7.0)*(pow(4.0,3.0+c)-5.0*d);
+	printf("%.3f\n",ans);
+	
+	ans = sqrt((2.0 + 8.0*b) / a);
+	printf("%.3f\n",ans);
+	
+	ans = cbrt(b*b-4*d);
+	printf("%.3f\n",ans);
+	
+}
